baae15ca32d942440702468f82e1acf4d5bde208
dev-avk <dev.avk777@gmail.com>
2025-03-18 17:07:49 +0300

    refactor: update test file

--- src/test.ts
+++ src/test.ts
@@ -2,8 +2,3 @@
 export function calculateTotal(numbers: number[]): number {
   return numbers.reduce((sum, num) => sum + num, 0)
 }
-
-// Duplicate implementation
-export function calculateTotal(numbers: number[]): number {
-  return numbers.reduce((sum, num) => sum + num, 0)
-}

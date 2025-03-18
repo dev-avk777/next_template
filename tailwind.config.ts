643c8e4375145b8161028ffc72c1d525700d5991
dev-avk <dev.avk777@gmail.com>
2025-03-18 23:17:05 +0300

    refactor: update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,25 +10,6 @@ const config: Config = {
   darkMode: 'class',
   theme: {
     extend: {
-      fontFamily: {
-        sans: [
-          'Inter',
-          'ui-sans-serif',
-          'system-ui',
-          '-apple-system',
-          'BlinkMacSystemFont',
-          'Segoe UI',
-          'Roboto',
-          'Helvetica Neue',
-          'Arial',
-          'Noto Sans',
-          'sans-serif',
-          'Apple Color Emoji',
-          'Segoe UI Emoji',
-          'Segoe UI Symbol',
-          'Noto Color Emoji',
-        ],
-      },
       colors: {
         ...palette,
         background: 'var(--background)',
